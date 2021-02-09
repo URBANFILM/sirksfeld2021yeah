e3c3ee8c8514a5307576290c01646eab2f6e1879
URBANFILM <78686633+URBANFILM@users.noreply.github.com>
2021-02-09 18:40:10 +0100

    Add files via upload

---
+++ fire.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fire" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="fire.png" width="272" height="272"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="56" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
